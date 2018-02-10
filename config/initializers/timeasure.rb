28a26f67a0524347396ffca8d981a4a79cae2e72
Eliav Lavi <eliavlavi@gmail.com>
2018-02-10 19:34:26 +0200

    remove post measuring proc as it is not necessary anymore in with-profiler gem

--- config/initializers/timeasure.rb
+++ config/initializers/timeasure.rb
@@ -1,10 +1,6 @@
 require 'timeasure'
 
 Timeasure.configure do |configuration|
-  configuration.post_measuring_proc = lambda do |measurement|
-    Profiling::Profiler.report_method_call(measurement.klass_name, measurement.method_name, measurement.t0, measurement.t1)
-  end
-
   configuration.rescue_proc = lambda do |e, base_class|
     Rails.logger.error "Timeasure failed upon calling configuration.post_measuring_proc for class #{base_class}.
     Error: #{e}"
